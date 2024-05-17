bb14b643c64188511f2c7c94208c3bea01822ae5
Sachin Awati <awatisachin021@gmail.com>
2024-05-18 02:18:15 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Sachin",
+    author_email="awatisachin021@gmail.com",
+    packages=find_packages()
+)
