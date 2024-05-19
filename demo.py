74346a99c8a42aa47e710608e5d00fee3b274487
Sachin Awati <awatisachin021@gmail.com>
2024-05-19 15:29:26 +0530

    exception added

---
+++ demo.py
@@ -0,0 +1,10 @@
+from us_visa.logger import logging
+
+from us_visa.exception import USVisaException
+import sys
+
+
+try:
+    a = 1 / '10'
+except Exception as e:
+    raise USVisaException(e, sys) from e
