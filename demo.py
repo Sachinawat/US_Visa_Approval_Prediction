1ce79306c15afa61f9fa4bf3b0cb2bcf2c953de6
Sachin Awati <awatisachin021@gmail.com>
2024-05-19 17:18:42 +0530

    exception added

--- demo.py
+++ demo.py
@@ -1,10 +1,12 @@
 from us_visa.logger import logging
 
-from us_visa.exception import USVisaException
+
+from us_visa.exception import USvisaException
 import sys
 
 
 try:
     a = 1 / '10'
 except Exception as e:
-    raise USVisaException(e, sys) from e
+    logging.info(e)
+    raise USvisaException(e, sys) from e
